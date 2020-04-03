00b0339f7a078259a569bffdad80905b86beeba9
Elizabeth Bowden <embowden05@gmail.com>
2020-04-03 13:24:30 -0700

    Add arrAvg to functions.js

---
+++ javascripts/functions.js
@@ -0,0 +1,13 @@
+//jshint esversion: 6
+// Req. 3 b
+let arrAvg = arr => {
+  let sum = 0,
+    avg;
+
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+
+  avg = sum / arr.length;
+  return avg;
+};
